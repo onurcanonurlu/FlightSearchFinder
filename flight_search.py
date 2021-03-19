978e649a17fe901ca4bbe9f3c62812a752c4844e
Onurcan Onurlu <onurcan.onurlu@gmail.com>
2021-03-19 19:17:26 +0300

    Create flight_search.py

---
+++ flight_search.py
@@ -0,0 +1,18 @@
+import requests
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com/v2/search"
+TEQUILA_API_KEY = "YOUR TEQUILA API KEY"
+
+class FlightSearch:
+    def get_destination_code(self, city_name):
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
+        return code
+
+    def search_for_flights(self,search_data):
+        for item in search_data:
+            response = requests.get(url=TEQUILA_ENDPOINT, params=item)
+            return response.json()
